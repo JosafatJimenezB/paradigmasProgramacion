580e78cbfc067ded0820ca77357903f9e5e3e858
josafat <josa221001@gmail.com>
2023-03-03 19:18:51 -0600

    add: hilos en java

---
+++ src/main/java/org/example/Cliente.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Cliente {
+
+    private String nombre;
+    private int[] CarroCompra;
+
+    public Cliente(String nombre, int[] carroCompra) {
+        this.nombre = nombre;
+        CarroCompra = carroCompra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int[] getCarroCompra() {
+        return CarroCompra;
+    }
+
+    public void setCarroCompra(int[] carroCompra) {
+        CarroCompra = carroCompra;
+    }
+}
