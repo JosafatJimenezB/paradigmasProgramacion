50f03a8fa44b83a2a5cd93effd0f0a494b3a9540
josafat <josa221001@gmail.com>
2023-03-10 10:49:49 -0600

    add: añadiendo primer trabajo con hilos

---
+++ src/main/java/org/example/IntroHilos/ThreadEjemplo2.java
@@ -0,0 +1,17 @@
+package org.example.IntroHilos;
+
+public class ThreadEjemplo2 implements Runnable {
+     public void run(){
+         for (int i = 0; i < 10; i++){
+             System.out.println(i + " " + Thread.currentThread().getName());
+             System.out.println("Termina el thread " + Thread.currentThread().getName());
+         }
+     }
+
+    public static void main(String[] args) {
+        new Thread (new ThreadEjemplo2(),"Pepe").start();
+        new Thread(new ThreadEjemplo2(),"Juan").start();
+        System.out.println("Termina el thread main");
+    }
+
+}
